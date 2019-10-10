c88bbaf6317bdc9dc9ddfe27034b026f8822ba87
fatfooot <fshubeilat@gmail.com>
2019-10-10 14:20:05 +0300

    Solved the first function using branch called (problem-1)

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
-}
-
-// Here is the test for sum(); uncomment it to run it
+    var c = a + b;
+    var resultString = 'The sum of ' + a + ' and ' + b + ' is ' + c + '.';
+    return [c , resultString];
+    }
+    
+sum(4, 7);
 // testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
