007f4ea40b836d49f2fb7d73b654990e0059fbc6
fatfooot <fshubeilat@gmail.com>
2019-10-11 15:50:12 +0300

    solving function 3

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ function sum(a, b) { //eslint-disable-line
     
 sum(4, 7);
 testSum(4, 7);
-<<<<<<< HEAD
-// saved it again, cause I did not do the testSum step
-=======
->>>>>>> problem-2
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -59,11 +55,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+var d = a + b + c;
+var e = a*b*c;
+    return [ d , e , a + ' and ' + b + ' and ' + c + ' sum to ' + d + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + e + '.'];
 }
-
+sumAndMultiply(4,7,5);
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
