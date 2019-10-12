1163aca133124d0ddfd85f598951f7c9cf89a133
fatfooot <fshubeilat@gmail.com>
2019-10-12 14:26:49 +0300

    solving function 5

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
+function sumArray(sumArr) { 
     var sum_total = 0;
    
     for(var i = 0; i < sumArr.length ; i++){
@@ -108,11 +108,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    var mul_total = 1;
 
+    for (var i = 0; i < multArr.length ; i++){
+        mul_total = multiply(mul_total, multArr[i])[0];
+    }
+    return [mul_total, 'The numbers ' + multArr + ' have a product of ' + mul_total + '.'];
 }
-
-// Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+//console.log(multiplyArray(testArray));
+// multiplyArray(); uncomment it to run it
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
