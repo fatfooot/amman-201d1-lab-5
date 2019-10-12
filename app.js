500e49c02729d5e2cfd4de4f16e565811a9a0cb6
fatfooot <fshubeilat@gmail.com>
2019-10-12 15:09:46 +0300

    I also assumed this function will receive an array with any length so here I made it for 3 elements of an array, and FYI I commented the other dynamic functions for this problem and problem number 4 too

--- app.js
+++ app.js
@@ -67,16 +67,7 @@ testSumAndMultiply(4,7,5);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
-/* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-
-// Write your code here
+/* Problem 4 */
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { 
@@ -91,20 +82,41 @@ function sumArray(sumArr) {
 // console.log(sumArray(testArray));
 
 testSumArray(testArray);
+/* And we can create it using the for loop too
+var testArray = [2, 3, 4]; //eslint-disable-line
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+function sumArray(sumArr) { 
+    var sum_total = 0;
+   
+    for(var i = 0; i < sumArr.length ; i++){
+      sum_total = sum(sum_total, sumArr[i])[0];
+    }
+   
+    return [sum_total ,sumArr +' was passed in as an array of numbers, and ' + sum_total + ' is their sum.'] ;
+
+    
+}
+*/
+
 
 /////////////////////////////////////
-/* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+/* Problem 5 */ 
 
-"The numbers 2,3,4 have a product of 24."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
+function multiplyArray(multArr) { //eslint-disable-line
+    var mul_total = 1;
+   mul_total = multiply(multArr[0], multArr[1])[0];
+   mul_total = multiply(mul_total, multArr[2])[0];
+    
+   return [mul_total, 'The numbers ' + multArr + ' have a product of ' + mul_total + '.'];
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+    
+}
+//console.log(multiplyArray(testArray));
+// multiplyArray(); uncomment it to run it
+ testMultiplyArray(testArray);
 
-// Write your code here
+ /*
 function multiplyArray(multArr) { //eslint-disable-line
     var mul_total = 1;
 
@@ -112,16 +124,8 @@ function multiplyArray(multArr) { //eslint-disable-line
         mul_total = multiply(mul_total, multArr[i])[0];
     }
     return [mul_total, 'The numbers ' + multArr + ' have a product of ' + mul_total + '.'];
-}
-//console.log(multiplyArray(testArray));
-// multiplyArray(); uncomment it to run it
- testMultiplyArray(testArray);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
-
-// You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
-
-// Don't forget to create a new branch for your work on the next question, if you attempt it.
+} 
+*/
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
